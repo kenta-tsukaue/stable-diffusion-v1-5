830875d8667a65ea99e070c541843b4017c48093
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-07-03 19:39:30 +0900

    promptをinputに

--- stable-diffusion-v1-5.py
+++ stable-diffusion-v1-5.py
@@ -5,8 +5,9 @@ model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-file_name = input("ファイル名を教えてね")
-prompt = "a photo of an astronaut riding a horse on mars"
+
+file_name = input("ファイル名を教えてね\n")
+prompt = input("どんな画像が欲しいかを教えてね\n")
 image = pipe(prompt).images[0]  
     
 image.save(file_name + ".png")
